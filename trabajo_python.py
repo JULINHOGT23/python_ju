b85b79c3abbf4a51ddf1c3e158e29bae914f430f
JULINHOGT23 <1485840@senati.pe>
2023-02-23 11:12:05 -0500

    pyton suma y resta actulizado3

--- trabajo_python.py
+++ trabajo_python.py
@@ -1,4 +1,4 @@
-#
+# desarrollo de un algoridmo de la suma y multiplicación con dos varibles o números
 
 # ingresmos los valores a restar y sumar
 int(input("ingrese el primer valor 1 : "))
@@ -7,6 +7,9 @@ int(input("ingrese el primer valor 2 : "))
 # calculamos los valores de la suma
 suma = v1 + v2
 
-# calculamos los valors dela resta
-resta = v1 - v2
+# calculamos los valors dela multiplicacion
+multiplicacion = v1 * v2
 
+# mostramos los resultados del algoridmo
+print("la suma de valores", suma)
+print("la resta de valores", multiplicacion)
