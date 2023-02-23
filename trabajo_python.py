6e27b05d0a23e460c28812dd315edfea2304815e
JULINHOGT23 <1485840@senati.pe>
2023-02-23 11:08:12 -0500

    pyton suma y resta actulizado2

--- trabajo_python.py
+++ trabajo_python.py
@@ -3,3 +3,10 @@
 # ingresmos los valores a restar y sumar
 int(input("ingrese el primer valor 1 : "))
 int(input("ingrese el primer valor 2 : "))
+
+# calculamos los valores de la suma
+suma = v1 + v2
+
+# calculamos los valors dela resta
+resta = v1 - v2
+
