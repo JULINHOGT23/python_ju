c945564969172cec15e9504f2c021c8904d815ca
JULINHOGT23 <125669103+JULINHOGT23@users.noreply.github.com>
2023-02-24 10:21:27 -0500

    Add files via upload

---
+++ algoritmo_suma_del _número_acual_mas_el-anterior.py
@@ -0,0 +1,4 @@
+# crear un programa que genere la suma del número actual y anterior 10 veces
+
+# solicitamos que se ingesen la cantidad de seris qu se ingesaran
+n = int(input("ingrese la cantidad de series : "))
