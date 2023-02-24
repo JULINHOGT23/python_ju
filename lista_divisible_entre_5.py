94de0615a1910c249299eada9041f6207cd6b490
JULINHOGT23 <1485840@senati.pe>
2023-02-24 18:13:58 -0500

    comprobacion de los numeros divisibles por 5

--- lista_divisible_entre_5.py
+++ lista_divisible_entre_5.py
@@ -3,4 +3,6 @@ números = [2, 5, 8, 10, 12, 15, 18, 20, 35, 48]
 
 # interamos sobre cada número en la lista
 for número in números:
-    
+    # si el número es divisible por 5, su residuo es 0
+    if número / 5 == 0:
+        
