22b0329047354f0ef4dc34e1776392a844e0076d
JULINHOGT23 <125669103+JULINHOGT23@users.noreply.github.com>
2023-02-24 10:27:11 -0500

    Delete algoritmo_suma_del _número_acual_mas_el-anterior.py

--- algoritmo_suma_del _número_acual_mas_el-anterior.py
+++
@@ -1,4 +0,0 @@
-# crear un programa que genere la suma del número actual y anterior 10 veces
-
-# solicitamos que se ingesen la cantidad de seris qu se ingesaran
-n = int(input("ingrese la cantidad de series : "))
