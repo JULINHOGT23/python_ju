a194953f872a2fb69a36fa99bf08979f6c72269f
JULINHOGT23 <125669103+JULINHOGT23@users.noreply.github.com>
2023-02-24 17:53:26 -0500

    Update lista_divisible_entre_5.py

--- lista_divisible_entre_5.py
+++ lista_divisible_entre_5.py
@@ -1,8 +1,1 @@
-# Creamos una lista de números
-numeros = [10, 15, 14, 8, 32, 20, 25, 30, 35, 40]
 
-# Iteramos sobre cada número en la lista
-for numero in numeros:
-    # Si el número es divisible por 5 (su módulo es 0), lo imprimimos
-    if numero % 5 == 0:
-        print(numero)
