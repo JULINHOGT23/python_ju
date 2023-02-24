6f33594233b0ba3d2a1286d981c2c110bea29d56
JULINHOGT23 <1485840@senati.pe>
2023-02-24 18:19:53 -0500

    imprimimos los numeros divisibles por 5

--- lista_divisible_entre_5.py
+++ lista_divisible_entre_5.py
@@ -4,5 +4,5 @@ números = [2, 5, 8, 10, 12, 15, 18, 20, 35, 48]
 # interamos sobre cada número en la lista
 for número in números:
     # si el número es divisible por 5, su residuo es 0
-    if número / 5 == 0:
-        
+    if número % 5 == 0:
+        print(número)
